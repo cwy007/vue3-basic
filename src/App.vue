375b8103d3bddc4786b5c36ee2db6636ca68efca
Viking Zhang <vikingmute@gmail.com>
2020-06-23 10:33:00 +0800

    add reactive lesson

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,30 @@
 </template>
 
 <script lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, reactive, toRefs } from 'vue'
+interface DataProps {
+  count: number;
+  double: number;
+  increase: () => void;
+}
 export default {
   name: 'App',
   setup() {
-    const count = ref(0)
-    const double = computed(() => {
-      return count.value * 2
+    // const count = ref(0)
+    // const double = computed(() => {
+    //   return count.value * 2
+    // })
+    // const increase = () => {
+    //   count.value++
+    // }
+    const data: DataProps  = reactive({
+      count: 0,
+      increase: () => { data.count++},
+      double: computed(() => data.count * 2)
     })
-    const increase = () => {
-      count.value++
-    }
+    const refData = toRefs(data)
     return {
-      count,
-      increase,
-      double
+      ...refData
     }
   }
 };
